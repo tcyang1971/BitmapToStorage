7a82bf7efba3b9b33b35eb24823675985e74b1c5
tcyang <tcyang@gm.pu.edu.tw>
2021-05-20 10:43:39 +0800

    將圖片轉成Bitmap再顯示

--- app/src/main/java/tw/edu/pu/csim/tcyang/bitmaptostorage/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/bitmaptostorage/MainActivity.kt
@@ -1,11 +1,22 @@
 package tw.edu.pu.csim.tcyang.bitmaptostorage
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var img: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        img = findViewById(R.id.img)
+        //val bmp: Bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pict_s)
+        val bmp: Bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pict)
+        img.setImageBitmap(bmp)
     }
 }
