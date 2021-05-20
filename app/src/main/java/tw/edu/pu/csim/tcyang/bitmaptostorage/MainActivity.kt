176c9d1c3097da4b46fb1067e36e3ec677ba17d2
tcyang <tcyang@gm.pu.edu.tw>
2021-05-20 11:47:45 +0800

    使用Storage上傳圖檔

--- app/src/main/java/tw/edu/pu/csim/tcyang/bitmaptostorage/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/bitmaptostorage/MainActivity.kt
@@ -6,9 +6,14 @@ import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.module.AppGlideModule
 import com.bumptech.glide.request.target.CustomTarget
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 @GlideModule
 public final class MyAppGlideModule : AppGlideModule()
@@ -33,10 +38,36 @@ class MainActivity : AppCompatActivity() {
                     override fun onResourceReady(resource: Bitmap,
                                                  transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?) {
                         img.setImageBitmap(resource)
+                        SaveToStorage(resource)
                     }
 
                     override fun onLoadCleared(placeholder: Drawable?) {
                     }
                 })
+    }
+
+    fun SaveToStorage(bmp:Bitmap){
+        //將圖片換成byteArray
+        val baos = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+        val pictData = baos.toByteArray()
+
+        //val filename = "images/pict.jpg"  //設定子節點與檔名
+        //根據系統時間設定檔名
+        val sdf = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS")  //定義時間格式
+        val dt = Date()  //取得現在時間
+        val dts = sdf.format(dt) //將目前日期轉為字串
+        val filename = "images/" + dts + ".jpg"  //設定子節點與檔名
+
+        val reference = FirebaseStorage.getInstance().getReference().child(filename)
+        //上傳到Firebase
+        reference.putBytes(pictData)
+                .addOnSuccessListener {
+                    Toast.makeText(baseContext, "上傳成功", Toast.LENGTH_SHORT).show()
+                }
+
+                .addOnFailureListener {
+                    Toast.makeText(baseContext, "上傳失敗", Toast.LENGTH_SHORT).show()
+                }
     }
 }
