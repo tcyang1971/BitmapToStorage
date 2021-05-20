d29d0d85a3ea053aec63f1deeb828b8b6baf1817
tcyang <tcyang@gm.pu.edu.tw>
2021-05-20 10:46:26 +0800

    改採Glide將圖片轉成Bitmap顯示

--- app/src/main/java/tw/edu/pu/csim/tcyang/bitmaptostorage/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/bitmaptostorage/MainActivity.kt
@@ -2,9 +2,16 @@ package tw.edu.pu.csim.tcyang.bitmaptostorage
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.target.CustomTarget
+
+@GlideModule
+public final class MyAppGlideModule : AppGlideModule()
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +23,20 @@ class MainActivity : AppCompatActivity() {
 
         img = findViewById(R.id.img)
         //val bmp: Bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pict_s)
-        val bmp: Bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pict)
-        img.setImageBitmap(bmp)
+        //val bmp: Bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.pict)
+        //img.setImageBitmap(bmp)
+
+        GlideApp.with(this)
+                .asBitmap()
+                .load(R.drawable.pict)
+                .into(object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap,
+                                                 transition: com.bumptech.glide.request.transition.Transition<in Bitmap>?) {
+                        img.setImageBitmap(resource)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                    }
+                })
     }
 }
